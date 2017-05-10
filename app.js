120f8a3b613170947a55b6227a8a6d2a7d6c5c37
Haidee <haideram97@gmail.com>
2017-05-09 23:08:47 -0500

    v1.0 agregadndo autenticacion

---
+++ app.js
@@ -0,0 +1,90 @@
+(function() {
+   // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyB7dw49nHdAJcYFWVI-vFLrt_kf2_xBl0w",
+    authDomain: "restaurante-522b9.firebaseapp.com",
+    databaseURL: "https://restaurante-522b9.firebaseio.com",
+    projectId: "restaurante-522b9",
+    storageBucket: "restaurante-522b9.appspot.com",
+    messagingSenderId: "136888241015"
+  };
+  firebase.initializeApp(config);
+  // body... 
+  // Get a reference to the database service
+  const databaseRef = firebase.database().ref('comments');
+  //GET ELEMENTS FORM
+  const btnSignUp= document.getElementById("btnLogin");
+  const form = document.querySelector("form");
+  //EVENTS
+  btnSignUp.addEventListener('click', e =>{    
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("profile");
+    provider.addScope("email");
+    firebase.auth().signInWithPopup(provider).then((result) =>{
+      // The firebase.User instance:
+      var user = result.user;
+      // The Facebook firebase.auth.AuthCredential containing the Facebook
+      // access token:
+      var credential = result.credential;
+        console.log(`${result.user.email} ha iniciado sesion`)
+        console.log(`${result.user.displayName} ha iniciado sesion`)
+        firebase.database().ref('users/' + result.user.displayName).update({
+          username: result.user.displayName,
+          email: result.user.email,
+          foto: result.user.photoURL
+        })
+      })
+      .catch(error => console.error(`Error : ${error.code}: ${error.message}`))
+  })
+
+  form.addEventListener("submit", postComment);
+
+  function login() {
+
+    // body...
+  }
+  
+  //Anonimus function timeStamp, par aagregar fecha y hora al comentario
+  const timeStamp = () => {
+  let options = {
+    month: '2-digit',
+    day: '2-digit',
+    year: '2-digit',
+    hour: '2-digit',
+    minute:'2-digit'
+  };
+  let now = new Date().toLocaleString('es-MX', options);
+  return now;
+  };
+
+
+  function postComment(e) {
+  e.preventDefault();
+  //obtiene los valores de los elementos con el id name y comment.
+  let name = document.getElementById("name").value;
+  let comment = document.getElementById("comment").value;
+  //Si las variables name y comment son diferente de nulas
+  if (name && comment) {
+    databaseRef.push({
+      name: name,
+      comment: comment,
+      time: timeStamp()
+    });
+  }
+
+  document.getElementById("name").value = '';
+  document.getElementById("comment").value = '';
+  };
+
+  databaseRef.on("child_added", snapshot => {
+  let comment = snapshot.val();
+  addComment(comment.name, comment.comment, comment.time);
+  });
+
+  const addComment = (name, comment, timeStamp) => {
+  let comments = document.getElementById("comments");
+  comments.innerHTML = `<hr> <span class='right'>${timeStamp}</span> <h6 id="nameUser">${name}</h6> <p id="comentarioUser">${comment}</p>${comments.innerHTML}`;
+  }
+
+}());
+ 
