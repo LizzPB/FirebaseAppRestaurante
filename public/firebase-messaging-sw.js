722b66f095728193d9007b9e893d7f0c382fc8eb
LizzPB <perezb.lizbeth@gmail.com>
2017-05-11 07:58:52 -0500

    chat

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not want to serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts('/__/firebase/3.8.0/firebase-app.js');
+importScripts('/__/firebase/3.8.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
